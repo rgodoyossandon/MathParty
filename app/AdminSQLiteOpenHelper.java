34d05c81832e4a2d8a1a6bcb833488e169b4bfe9
Rodrigo Godoy <rgodoyossandon@gmail.com>
2023-12-06 20:28:01 -0300

    Creacion del proyecto

---
+++ app/AdminSQLiteOpenHelper.java
@@ -0,0 +1,2 @@
+public class AdminSQLiteOpenHelper {
+}
---
+++ app/src/main/java/com/example/mathparty/Main2Activity_Nivel2.java
@@ -0,0 +1,14 @@
+package com.example.mathparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity_Nivel2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_nivel2);
+    }
+}
